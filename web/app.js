55077a24d8a753bc61d620a5f2bd2081ba03f7cb
qbang <vhfhddl0101@naver.com>
2019-08-05 16:23:33 +0900

    0805

---
+++ web/app.js
@@ -0,0 +1,6 @@
+var greetfrom = function(){
+    console.log("실행: ", req.body.friendID);
+    // 인스타그램 요청보내기
+}
+
+module.exports = greetfrom;
--- web/server.js
+++ web/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var router = require('./router/main')(app);
+var greet = require('./app');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,3 +13,10 @@ var server = app.listen(3000, function(){
 });
 
 app.use(express.static('views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.post('/app', function(req,res){
+    greet();
+    res.send("Hi");
+});
